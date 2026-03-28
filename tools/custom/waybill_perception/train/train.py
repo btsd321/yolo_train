9498a82d216af59e3fe064cc8327b57274d8d8be
btsd321 <1123347532@qq.com>
2026-03-28 22:06:42 +0800

    feat: 添加数据集和模型配置文件，更新训练脚本以支持新的数据集路径和参数

--- tools/custom/waybill_perception/train/train.py
+++ tools/custom/waybill_perception/train/train.py
@@ -1,21 +1,22 @@
 from ultralytics import YOLO
 
-dataset_config_file = f"/home/lixinlong/Project/yolo_train/Data/dataset/waybill_perception/dataset_config.yaml"
-model_config_file = f"/home/lixinlong/Project/yolo_train/Data/dataset/waybill_perception/yolo26m-obb.yaml"
+if __name__ == '__main__':
+    dataset_config_file = f"D:/Project/yolo_train/tools/custom/waybill_perception/config/dataset.yaml"
+    model_config_file = f"D:/Project/yolo_train/tools/custom/waybill_perception/config/yolo26m-obb.yaml"
 
-# Load a COCO-pretrained YOLO26n model
-model = YOLO(model_config_file, task="obb")
+    # Load a COCO-pretrained YOLO26n model
+    model = YOLO(model_config_file, task="obb")
 
-# Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data=dataset_config_file, 
-                      epochs=300, 
-                      imgsz=960,
-                      batch=16,
-                      device=0,
-                      project="/home/lixinlong/Project/yolo_train/Data/output/waybill_perception",
-                      name="yolo26m-obb",
-                      exist_ok=True
-                      )
+    # Train the model on the COCO8 example dataset for 100 epochs
+    results = model.train(data=dataset_config_file,
+                          epochs=300,
+                          imgsz=640,
+                          batch=16,
+                          device=0,
+                          project="output",
+                          name="waybill_perception_obb",
+                          exist_ok=True
+                          )
 
-# Run inference with the YOLO26n model on the 'bus.jpg' image
-results = model("path/to/bus.jpg")
+    # # Run inference with the YOLO26n model on the 'bus.jpg' image
+    # results = model("path/to/bus.jpg")
