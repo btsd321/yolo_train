b57e45ef49f842e3ce3d62726cca14e06138260d
btsd321 <1123347532@qq.com>
2026-05-27 09:34:44 +0800

    更新seg训练脚本

---
+++ tools/custom/linden_perception/train/train_seg.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    dataset_config_file = f"D:/Project/yolo_train/tools/custom/waybill_perception/config/dataset.yaml"
+    model_config_file = f"D:/Project/yolo_train/tools/custom/waybill_perception/config/yolo26m-obb.yaml"
+
+    # Load a COCO-pretrained YOLO26n model
+    model = YOLO(model_config_file, task="segment")
+
+    # Train the model on the COCO8 example dataset for 100 epochs
+    results = model.train(data=dataset_config_file,
+                          epochs=300,
+                          imgsz=640,
+                          batch=16,
+                          device=0,
+                          project="output",
+                          name="linden_perception_seg",
+                          exist_ok=True
+                          )
+
+    # # Run inference with the YOLO26n model on the 'bus.jpg' image
+    # results = model("path/to/bus.jpg")
